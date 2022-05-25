6affa8e562962a89001f1f1cec8b48bd5923d2fc
Almustapha Muhammad Sunusi <52855309+Mustapha0611@users.noreply.github.com>
2022-05-25 21:32:46 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+//   Menu Animation
+var burger = document.querySelector(".burg");
+var close = document.querySelector(".close");
+
+burger.addEventListener("click", () => {
+  anime({
+    targets: "nav",
+    translateX: "100%",
+    duration: 300,
+    easing: "linear",
+  });
+});
+
+close.addEventListener("click", () => {
+  anime({
+    targets: "nav",
+    translateX: "-100%",
+    duration: 300,
+    easing: "linear",
+  });
+});
+
